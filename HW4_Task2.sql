302ca1cfa6bab1f7cacb99c88034bacdc8a18089
Alona <92216229+alona-mandziuk@users.noreply.github.com>
2023-02-03 21:13:22 +0200

    Add files via upload
    Використовуючи JOIN’s та ShopDB отримати імена покупців та працівників, у яких TotalPrice товару більше 1000.

---
+++ HW4_Task2.sql
@@ -0,0 +1,20 @@
+/*Використовуючи JOIN’s та ShopDB отримати імена покупців та працівників, у яких TotalPrice товару більше 1000.*/
+
+use ShopDB;
+
+select Customers.FName, Customers.LName, Employees.FName, Employees.LName, SUM(TotalPrice)
+from Employees
+ join Orders
+on Employees.EmployeeID = Orders.EmployeeID
+ join Customers
+on Orders.CustomerNo = Customers.CustomerNo
+ join OrderDetails 
+on Orders.OrderID =  OrderDetails.OrderID
+group by  Employees.FName,
+		  Employees.LName,
+          Customers.FName,
+          Customers.LName
+having SUM(TotalPrice) > 1000;
+
+
+
